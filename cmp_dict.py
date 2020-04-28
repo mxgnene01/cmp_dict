4c7e2ad8feb86e56d968a7689b613d6d0dd35664
孟祥国 <mxgnene01@gmail.com>
2020-04-28 10:58:07 +0800

    1. fix bug 2. 对list 增加排序的控制

--- cmp_dict.py
+++ cmp_dict.py
@@ -11,8 +11,12 @@
 # +--+--+--+--+--+--+--+--+--+--+--+--+--+
 
 
-def cmp_dict(src_data, dst_data, path='', diff=dict()):
+def cmp_dict(src_data, dst_data, path='', diff=dict(), sort=False):
     # assert type(src_data) == type(dst_data), "type: '{}' != '{}'".format(type(src_data), type(dst_data))
+    # 这个地方为了解决：pytest.mark.parametrize 多case中一个失败，导致后续case也失败的问题
+    if path == '':
+         diff = dict()
+
     if isinstance(src_data, dict):
         for key in src_data:
             newpath = path + '.' + key
@@ -21,8 +25,15 @@ def cmp_dict(src_data, dst_data, path='', diff=dict()):
             else:
                 diff[newpath] = [src_data.get(key), 'not exist is key']
     elif isinstance(src_data, list):
-        for k, v in enumerate(zip(src_data, dst_data)):
-            cmp_dict(v[0], v[1], path + '.' + str(k))
+        if len(src_data) == len(dst_data):
+            if sort:
+                tmp = zip(sorted(src_data), sorted(dst_data))
+            else:
+                tmp = zip(src_data, dst_data)
+            for k, v in enumerate(tmp):
+                diff = cmp_dict(v[0], v[1], path + '.' + str(k), diff)
+        else:
+            diff[path] = [src_data, dst_data]
     else:
         if src_data != dst_data:
             diff[path] = [src_data, dst_data]
@@ -31,8 +42,8 @@ def cmp_dict(src_data, dst_data, path='', diff=dict()):
 
 
 if __name__ == "__main__":
-    xx = {"a":"b", "version":"1.0","status":10,"errorMsg":"全部成功","elapsed":3,"data":{"groups":[{"name":"全部","id":-1,"dispOrd":11},{"name":"面部护肤","id":1142,"dispOrd":1}],"firstCategoryList":[{"name":"美妆个护","id":1041,"dispOrd":12},{"name":"食品生鲜","id":1004,"dispOrd":13}]}}
-    yy = {"version":"1.0","status":0,"errorMsg":"全部成功","elapsed":3,"data":{"groups":[{"name":"全部","id":-1,"dispOrd":1},{"name":"面部护肤","id":1042,"dispOrd":1}],"firstCategoryList":[{"name":"美妆个护","id":1041,"dispOrd":12},{"name":"食品生鲜","id":1004,"dispOrd":13}]}}
+    xx = {'kkk0':'vvv0', 'kkkk1': [{'c': 'd', 'k2': [{'kk2': 'vv2'}, {'kk3': 'vv3'}]}, {'k3': 'v3'}], 'kkkk2': 'vvvv2'}
+    yy = {'kkk0':'vvvv0', 'kkkk1': [{'c': 'd', 'k2': [{'kk2': 'diffvv2'}, {'kk3': 'vv3'}]}, {'k3': None}]}
     diff = cmp_dict(xx, yy)
     for k, v in diff.items():
         print('key is {} -> value is {}'.format(k, v))
